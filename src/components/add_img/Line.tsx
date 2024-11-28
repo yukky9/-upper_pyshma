84106e3927dbf5037fa2457f914b81a95b73b77b
yukky9 <kozhurinayulia@gmail.com>
2024-11-28 23:45:53 +0300

    final

--- src/components/add_img/Line.tsx
+++ src/components/add_img/Line.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Line = () => {
     return (
-        <div className='mt-40'>
+        <div className='mt-32'>
             <blockquote className=" fixed p-2 h-64 border-s-4 border-blue-900 rounded"/>
         </div>
     );

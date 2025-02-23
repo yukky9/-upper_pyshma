2dd9cc0ee3a1ddf110649aab16aa686985cbefd7
yukky9 <kozhurinayulia@gmail.com>
2025-02-23 23:03:35 +0300

    fix header

--- src/components/templates/Header.tsx
+++ src/components/templates/Header.tsx
@@ -11,7 +11,7 @@ const Header = () => {
             <nav className="bg-gray-100 h-16 mb-10">
                 <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl p-4">
                     <div className="grid grid-cols-2 items-center gap-1">
-                        <img className="h-10 w-10 ml-16 rounded" src={logo}/>
+                        <img className="h-10 w-10 ml-16 rounded" src={logo} alt='logo'/>
                         <span onClick={handleClick}
                             className="text-2xl font-semibold whitespace-nowrap cursor-pointer">Monitoring</span>
                     </div>

93823c6b854509bc6c881027472fb721edd5f468
yukky9 <kozhurinayulia@gmail.com>
2025-02-23 23:02:34 +0300

    logo

--- src/components/templates/Header.tsx
+++ src/components/templates/Header.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import logo from '../../app/logo/general_logo.png'
 
 const Header = () => {
     const handleClick = () => {
@@ -9,7 +10,8 @@ const Header = () => {
         <div>
             <nav className="bg-gray-100 h-16 mb-10">
                 <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl p-4">
-                    <div className="grid grid-cols-2 items-center">
+                    <div className="grid grid-cols-2 items-center gap-1">
+                        <img className="h-10 w-10 ml-16 rounded" src={logo}/>
                         <span onClick={handleClick}
                             className="text-2xl font-semibold whitespace-nowrap cursor-pointer">Monitoring</span>
                     </div>
